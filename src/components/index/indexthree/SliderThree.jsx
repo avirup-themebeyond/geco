c7899949685d3d97e8a02792463d5e9dfdbfbfc5
avirup-themebeyond <avirup.themebeyond@gmail.com>
2022-04-20 01:07:20 +0600

    first commit

---
+++ src/components/index/indexthree/SliderThree.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function SliderThree() {
+  return (
+	 <section className="third-banner-bg">
+            <div className="container custom-container">
+              <div className="row">
+                <div className="col-12">
+                  <div className="third-banner-img wow bounceInDown" data-wow-delay=".2s">
+                    <img src="assets/img/slider/third_banner_img.png" alt="" />
+                  </div>
+                  <div className="third-banner-content text-center wow bounceInUp" data-wow-delay=".2s">
+                    <h2>iám stream<span>er</span></h2>
+                    <h6>online streaming media</h6>
+                    <a href="/#" className="btn rotated-btn">buy Now</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+  )
+}
+
+export default SliderThree
